220a1d3cd9814d20b96617a0051c9772d402626b
tinegagideon <tinegagideon@gmail.com>
2020-06-17 10:14:26 -0700

    modified js

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
 const api = {
-    key: "4b84c48893285fb922690f8ae9d5e5b5",
-    baseurl:"https://api.openweathermap.org/data/2.5/"
-}
+  key: '4b84c48893285fb922690f8ae9d5e5b5',
+  baseurl: 'https://api.openweathermap.org/data/2.5/',
+};
 
 const searchBox = document.querySelector('.search-box');
-searchBox.addEventListener('keypress', setQuery)
+searchBox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
-    if (evt.keyCode == 13){
-        getResults(searchBox.value);
-  
-    }
+  if (evt.keyCode == 13) {
+    getResults(searchBox.value);
+  }
 }
 
-function getResults (query) {
-    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather =>{
-        return weather.json();
-    }).then(displayResults);
+function getResults(query) {
+  fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
 }
 
-function displayResults (weather) {
-    console.log(weather);
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}
     `;
-    let now = new Date();
-    let date = document.querySelector('.location .date');
-    date.innerText = dateBuilder(now);
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
 
-    let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
 
-    let weatherEl = document.querySelector('.current .weather');
-    weatherEl.innerText = weather.weather[0].main;
+  let weatherIcon = document.querySelector('.current .weather-icon');
+  weatherIcon.innerHTML = `<img src="icons/${weather.weather[0].icon}.png"/>`;
 
-    let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+  let weatherEl = document.querySelector('.current .weather');
+  weatherEl.innerText = weather.weather[0].main;
 }
 
-function dateBuilder(d){
-    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+function dateBuilder(d) {
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
-
+  return `${day} ${date} ${month} ${year}`;
 }
