60ae52c3ce88896dae4d78212be860a5ae5d64df
tinegagideon <tinegagideon@gmail.com>
2020-06-21 10:27:44 -0700

    modified files

--- script.js
+++ script.js
@@ -25,9 +25,6 @@ function displayResults(weather) {
   let city = document.querySelector('.location .city');
   city.innerText = `${weather.name}, ${weather.sys.country}
     `;
-  let now = new Date();
-  let date = document.querySelector('.location .date');
-  date.innerText = dateBuilder(now);
 
   let temp = document.querySelector('.current .temp');
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
@@ -37,37 +34,4 @@ function displayResults(weather) {
 
   let weatherEl = document.querySelector('.current .weather');
   weatherEl.innerText = weather.weather[0].main;
-}
-
-function dateBuilder(d) {
-  let months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  let days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
-
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
 }
